3bf01b9f69d5bf7c51dab2bd0b4c6f558c880ca1
Andrea Bonatti <andreabonatti92@gmail.com>
2021-11-30 11:58:42 +0100

    Spinner and some custom colors added

--- app/src/main/java/com/example/covidtracker/MainActivity.kt
+++ app/src/main/java/com/example/covidtracker/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.covidtracker
 
 import android.os.Bundle
 import android.util.Log
+import androidx.annotation.ColorInt
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
@@ -16,8 +18,10 @@ import java.util.*
 
 private const val BASE_URL = "https://api.covidtracking.com/v1/"
 private const val TAG = "MainActivity"
+private const val ALL_STATES = "ALL (Nationwide)"
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var currentlyShownData: List<CovidData>
     private lateinit var adapter: CovidSparkAdapter
     private lateinit var perStateDailyData: Map<String, List<CovidData>>
     private lateinit var nationalDailyData: List<CovidData>
@@ -71,13 +75,28 @@ class MainActivity : AppCompatActivity() {
                 // .reversed() to retrieve data from the holder to the newest
                 perStateDailyData = stateData.reversed().groupBy { it.state }
                 Log.i(TAG, "Update spinner with state names")
-                // TODO: Update spinner with state names
+                // Update spinner with state names
+                updateSpinnerWithStateData(perStateDailyData.keys)
             }
 
             override fun onFailure(call: Call<List<CovidData>>, t: Throwable) {
                 Log.e(TAG, "onFailure $t")
             }
         })
+    }
+
+    private fun updateSpinnerWithStateData(stateNames: Set<String>) {
+        val stateAbbreviationList = stateNames.toMutableList()
+        stateAbbreviationList.sort()
+        stateAbbreviationList.add(0, ALL_STATES)
+
+        // Add state list as data source for the spinner
+        spinnerSelect.attachDataSource(stateAbbreviationList)
+        spinnerSelect.setOnSpinnerItemSelectedListener { parent, _, position, _ ->
+            val selectedState = parent.getItemAtPosition(position) as String
+            val selectedData = perStateDailyData[selectedState] ?: nationalDailyData
+            updateDisplayWithData(selectedData)
+        }
     }
 
     private fun setupEventListeners() {
@@ -109,11 +128,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateDisplayMetric(metric: Metric) {
+        // Update the color of the chart
+        val colorRes = when (metric) {
+            Metric.NEGATIVE -> R.color.colorNegative
+            Metric.POSITIVE -> R.color.colorPositive
+            Metric.DEATH -> R.color.colorDeath
+        }
+        @ColorInt val colorInt = ContextCompat.getColor(this, colorRes)
+        sparkView.lineColor = colorInt
+        tvMetricLabel.setTextColor(colorInt)
+
+        // Updating the metric on the adapter
         adapter.metric = metric
         adapter.notifyDataSetChanged()
+
+        // Reset number and date shown in the bottom text views
+        updateInfoForDate(currentlyShownData.last())
     }
 
     private fun updateDisplayWithData(dailyData: List<CovidData>) {
+        currentlyShownData = dailyData
         // Create a a new SparkAdapter with data
         adapter = CovidSparkAdapter(dailyData)
         sparkView.adapter = adapter
